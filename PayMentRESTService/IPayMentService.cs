ccc0efb2979dc1dea822f7757594dfcec7c1e8a3
ece <eceercan91@hotmail.com>
2018-11-14 14:06:29 +0300

    Add project files.

---
+++ PayMentRESTService/IPayMentService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace PayMentRESTService
+{
+
+    [ServiceContract]
+    public interface IPayMentService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET",UriTemplate = "/PayBill/{PayId}", BodyStyle = WebMessageBodyStyle.Wrapped,RequestFormat = WebMessageFormat.Json,ResponseFormat = WebMessageFormat.Json)]
+        string PayBill(string PayId);
+       
+    }
+}
---
+++ PayMentRESTService/PayMentService.svc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace PayMentRESTService
+{
+    
+    public class PayMentService : IPayMentService
+    {
+        //concrete
+        public string PayBill(string PayId)
+        {
+            return "Transaction having PayId " + PayId + " was successful";
+        }
+    }
+}
